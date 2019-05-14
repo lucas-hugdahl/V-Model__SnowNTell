99bb12c79d6f6ed551b916263319168641764322
Lucas Hugdahl <lucashugdahl@Lucass-MacBook-Pro.local>
2019-05-14 14:47:19 -0700

    updated dependencies

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import store from './store';
 import GlobalComponents from "@/components/global/global.js";
 import BootstrapVue from 'bootstrap-vue'
 
@@ -21,6 +20,5 @@ import "./assets/scss/all.scss";
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app');
